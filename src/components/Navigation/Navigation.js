8a2bbee564946eec83f46d72918090a562c08221
Edmund Chan <edmund@nerb.com.sg>
2020-01-19 14:40:04 +0800

    Initial commit of Navigation component

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import roles from '../../settings/roles';
+import Typography from '@material-ui/core/Typography';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import MenuList from '@material-ui/core/MenuList';
+import MenuItem from '@material-ui/core/MenuItem';
+import Divider from '@material-ui/core/Divider';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  navMenuIcon: {
+    minWidth: 35
+  },
+  selected: {
+    color: theme.palette.primary.main
+  }
+}));
+
+const MainNav = props => {
+  const classes = useStyles();
+  const userRole = Cookies.get('user_role');
+  const { [userRole]: currentRole } = roles;
+  const links = currentRole.routes.map(({ icon: Icon, ...route }) => {
+    const link = React.forwardRef((props, ref) => (
+      <NavLink {...props} activeClassName={classes.selected} />
+    ));
+    return (
+      <MenuItem key={route.id} component={link} to={`${props.path}${route.path}`}>
+        <ListItemIcon classes={{ root: classes.navMenuIcon }}>
+          <Icon />
+        </ListItemIcon>
+        <Typography>{route.title}</Typography>
+      </MenuItem>
+    );
+  });
+  return (
+    <>
+      {/* <Avatar alt="Edmund" src={ProfileAvatar} className={classes.profileAvatar} /> */}
+      <Divider />
+      <MenuList>{links}</MenuList>
+    </>
+  );
+};
+export default MainNav;
