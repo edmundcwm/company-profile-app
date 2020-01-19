02855096459cf8a097a3922eb58cc37f9d964721
Edmund Chan <edmund@nerb.com.sg>
2020-01-19 16:43:26 +0800

    Remove unused import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import Login from './components/Login/Login';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import PrivateRoute from './hoc/PrivateRoute/PrivateRoute';
-import Cookies from 'js-cookie';
 import Main from './components/Main';
 import { CompanyProfileProvider } from './components/Context/CompanyProfileContext';
 import { OrderProvider } from './components/Context/OrderContext';
