7c8e1803584244ab5abefa54ecaa3a1f733f377d
Edmund Chan <edmund@nerb.com.sg>
2020-01-19 16:41:01 +0800

    Initial commit of UserDashboard component

---
+++ src/components/UserDashboard/UserDashboard.js
@@ -0,0 +1,119 @@
+import React, { useState, useEffect, useContext } from 'react';
+import axios from 'axios';
+import { UserServices } from '../../services/userServices/userServices';
+import Paper from '@material-ui/core/Paper';
+import Orders from './Orders';
+import { OrderContext } from '../Context/OrderContext';
+
+const endpoint = 'nerbcrmwp/v1/orders/';
+const CancelToken = axios.CancelToken;
+
+export default function UserDashboard(props) {
+  const [isLoading, setIsLoading] = useState(true);
+  const { orderData, setOrderData, fetchOrders, setFetchOrders } = useContext(OrderContext);
+  const [error, setError] = useState('');
+  const source = CancelToken.source();
+  useEffect(() => {
+    if (fetchOrders) {
+      UserServices.getByEmail(source.token, endpoint)
+        .then(response => {
+          if (response.data.length) {
+            setOrderData(response.data);
+          }
+          // disable fetching of orders on re-render
+          setFetchOrders(false);
+          setIsLoading(false);
+        })
+        .catch(error => {
+          if (axios.isCancel(error)) {
+            console.log('request cancelled');
+          } else {
+            console.log(error);
+            setError('Failed to fetch data. Please contact support.');
+            setIsLoading(false);
+          }
+        });
+    } else {
+      setIsLoading(false);
+    }
+    return () => {
+      source.cancel();
+    };
+  }, [fetchOrders, setFetchOrders, setOrderData, source]);
+
+  return (
+    <>
+      {isLoading ? (
+        <div>
+          <p>Retrieving orders...</p>
+        </div>
+      ) : (
+        <div>
+          {error || (
+            <Paper>
+              <Orders />
+            </Paper>
+          )}
+        </div>
+      )}
+    </>
+  );
+}
+
+// class UserDashboard extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       data: [],
+//       isLoading: true
+//     };
+//     this.source = CancelToken.source();
+//   }
+
+//   componentDidMount() {
+//     if (!dataStore.cache.userOrdersCached) {
+//       const endpoint = 'nerbcrmwp/v1/orders/';
+
+//       UserServices.getByEmail(this.source.token, endpoint)
+//         .then(response => {
+//           dataStore.data.userOrders = response.data;
+//           if (!dataStore.data.userOrders.length) {
+//             errorMessage = 'No Orders found.';
+//           }
+//           this.setState({ isLoading: false });
+//           dataStore.cache.userOrdersCached = true;
+//         })
+//         .catch(error => {
+//           if (axios.isCancel(error)) {
+//             console.log('request cancelled');
+//           } else {
+//             console.log(error);
+//             errorMessage = 'Failed to fetch data. Please contact support.';
+//             this.setState({ isLoading: false });
+//           }
+//         });
+//     } else {
+//       this.setState({ isLoading: false });
+//     }
+//   }
+
+//   componentWillUnmount() {
+//     this.source.cancel();
+//   }
+
+//   render() {
+//     return this.state.isLoading ? (
+//       <div>
+//         <p>Retrieving orders...</p>
+//       </div>
+//     ) : (
+//       <div>
+//         <Paper>
+//           <Orders />
+//         </Paper>
+//       </div>
+//     );
+//   }
+// }
+
+// export default UserDashboard;
