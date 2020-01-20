5e613fa668e623788a152ef70026672cc22b544f
Edmund Chan <edmund@nerb.com.sg>
2020-01-20 17:23:54 +0800

    Adjust Grid styling

--- src/components/CompanyProfile/ProfileInfo/ProfileInfo.js
+++ src/components/CompanyProfile/ProfileInfo/ProfileInfo.js
@@ -2,19 +2,19 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Shareholders from './Shareholders';
+import Employees from './Employees';
 import Box from '@material-ui/core/Box';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import useCompanyProfileStyles from '../CompanyProfileStyles';
 
 export default function ProfileInfo(props) {
   const classes = useCompanyProfileStyles();
-
   return (
     <>
       <CssBaseline />
-      <Box width={500} mx="auto">
+      <Box width={1000} mx="auto">
         <Grid container spacing={3} alignItems="stretch">
-          <Grid item xs={12} sm={12}>
+          <Grid item xs={6}>
             <Paper className={classes.companyProfilePaper} square>
               <Shareholders
                 data={props.data.shareholders}
