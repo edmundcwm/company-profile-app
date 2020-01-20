078537e37c7dc0066733eeb0029f68a21f308410
Edmund Chan <edmund@nerb.com.sg>
2020-01-20 16:59:32 +0800

    Add Employees to fields for displaying

--- src/components/CompanyProfile/FieldsToDisplay.js
+++ src/components/CompanyProfile/FieldsToDisplay.js
@@ -11,6 +11,29 @@ const shareholders = [
   }
 ];
 
+const employees = [
+  {
+    label: 'Name',
+    value: 'employee_name',
+    component: 'p',
+    type: 'text'
+  },
+  {
+    label: 'Email',
+    value: 'employee_email',
+    component: 'p',
+    type: 'email'
+  },
+  {
+    label: 'Contract',
+    value: 'employee_contract_title',
+    component: 'p',
+    link: 'employee_contract_link',
+    type: 'text'
+  }
+];
+
 export const ProfileFields = {
-  shareholders
+  shareholders,
+  employees
 };
